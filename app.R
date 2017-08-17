c5cf37df208ecd9c15e8ae5cdf905aa5e553d199
Ruan van Mazijk <ruanvmazijk@gmail.com>
2017-08-17 12:24:43 +0200

    Converted to Python indentation style

--- app.R
+++ app.R
@@ -11,70 +11,51 @@ draw <- function(fun_y = "y",
                  lwt = 0.05,
                  x = seq(-1, 1, by = 0.02),  # i.e. in 100ths
                  y = seq(-1, 1, by = 0.02)) {
-
     fun_child <- function(x, y) {}
-    body(fun_child) <- parse(text = glue(
-        "({y} < {x} + {lwt}) && ({y} > {x} - {lwt})",
-        x = fun_x, y = fun_y, lwt = lwt
-    ))
-
+    body(fun_child) <-
+        parse(text = glue("({y} < {x} + {lwt}) && ({y} > {x} - {lwt})",
+                          x = fun_x,
+                          y = fun_y,
+                          lwt = lwt))
     z <- matrix(nrow = length(x), ncol = length(y))
     for (i in 1:nrow(z)) {
         for (j in 1:ncol(z)) {
-            z[nrow(z) + 1 - i, ncol(z) + 1 - j] <- fun_child(-x[i], y[j])
-        }
-    }
-
+            z[nrow(z) + 1 - i, ncol(z) + 1 - j] <- fun_child(-x[i], y[j])}}
     par(mar = c(0.2, 0.2, 0.2, 0))
-    plot(raster(t(z)), legend = FALSE, xaxt = "n", yaxt = "n")
-
-}
-
+    plot(raster(t(z)), legend = FALSE, xaxt = "n", yaxt = "n")}
 ui <- shinyUI(fluidPage(
     titlePanel("Graph functions using rasters"),
     sidebarLayout(
-        sidebarPanel(
-            # Formula input to draw
-            textInput(inputId = "formula_y", label = "LHS:", value = "y"),
-            textInput(inputId = "formula_x", label = "RHS:", value = "x"),
-            # X & Y plotting ranges
-            sliderInput(
-                inputId = "range_y",
-                label = "y-range",
-                min = -10,
-                max = 10,
-                value = c(-1, 1),
-                dragRange = TRUE
-            ),
-            sliderInput(
-                inputId = "range_x",
-                label = "x-range",
-                min = -10,
-                max = 10,
-                value = c(-1, 1),
-                dragRange = TRUE
-            )
-        ),
+        sidebarPanel(# Formula input to draw
+                     textInput(inputId = "formula_y",
+                               label = "LHS:",
+                               value = "y"),
+                     textInput(inputId = "formula_x",
+                               label = "RHS:",
+                               value = "x"),
+                    # X & Y plotting ranges
+                    sliderInput(inputId = "range_y",
+                                label = "y-range",
+                                min = -10,
+                                max = 10,
+                                value = c(-1, 1),
+                                dragRange = TRUE),
+                    sliderInput(inputId = "range_x",
+                                label = "x-range",
+                                min = -10,
+                                max = 10,
+                                value = c(-1, 1),
+                                dragRange = TRUE)),
         # Draw!
-        mainPanel(plotOutput("drawPlot"))
-    )
-))
-
+        mainPanel(plotOutput("drawPlot")))))
 server <- shinyServer(function(input, output) {
     output$drawPlot <- renderPlot({
-        draw(
-            fun_y = input$formula_y,
-            fun_x = input$formula_x,
-            x = seq(
-                input$range_x[1], input$range_x[2],
-                by = (input$range_x[2] - input$range_x[1]) / 100
-            ),
-            y = seq(
-                input$range_y[1], input$range_y[2],
-                by = (input$range_y[2] - input$range_y[1]) / 100
-            )
-        )
-    })
-})
-
+        draw(fun_y = input$formula_y,
+             fun_x = input$formula_x,
+             x = seq(from = input$range_x[1],
+                     to = input$range_x[2],
+                     by = (input$range_x[2] - input$range_x[1]) / 100),
+             y = seq(from = input$range_y[1],
+                     to = input$range_y[2],
+                     by = (input$range_y[2] - input$range_y[1]) / 100))})})
 shinyApp(ui = ui, server = server)
